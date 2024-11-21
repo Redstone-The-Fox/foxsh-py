6dc920bed5030c1919987da69d4514745650736f
arcticfox <arcticfox@fakeema.il>
2024-11-21 17:26:36 +0000

    0.1.5

--- foxsh.py
+++ foxsh.py
@@ -2,3 +2,19 @@
 
 # | Imports
 import os
+
+# | Functions
+...
+
+# | Main Function
+def main() -> None:
+    while True:
+        cmd = input('> ')
+        if cmd[:4] == 'echo':
+            if cmd[5] == '"' and cmd[-1] == '"' or cmd[5] == '\'' and cmd[-1] == '\'': print(cmd[6:-1])
+        elif cmd == 'clear':
+            os.system('clear')
+
+# | On Run
+if __name__ == '__main__':
+    main()
